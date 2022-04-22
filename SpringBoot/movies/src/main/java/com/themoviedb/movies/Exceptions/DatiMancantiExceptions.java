e554a8b93caf9d3bb36b0adf969c4c5de4d13237
stecerto <st.certo@gmail.com>
2022-04-22 11:04:23 +0200

    modifiche exceptions

--- SpringBoot/movies/src/main/java/com/themoviedb/movies/Exceptions/DatiMancantiExceptions.java
+++ SpringBoot/movies/src/main/java/com/themoviedb/movies/Exceptions/DatiMancantiExceptions.java
@@ -2,8 +2,7 @@ package com.themoviedb.movies.Exceptions;
 
 import com.themoviedb.movies.Model.Users;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.json.GsonBuilderUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Errore nei dati inseriti!")
--- SpringBoot/movies/src/main/java/com/themoviedb/movies/Model/Users.java
+++ SpringBoot/movies/src/main/java/com/themoviedb/movies/Model/Users.java
@@ -23,10 +23,10 @@ public class Users {
         return user_id;
     }
 
-   /* public void setUser_id(int user_id) {
+    public void setUser_id(int user_id) {
         this.user_id = user_id;
-    }*/
-
+    }
+//Costruttore vuoto
     public Users() {
     }
 
